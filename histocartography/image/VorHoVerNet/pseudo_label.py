2207eec5668f071b998b4b90684b959880ea6645
Ting An Yen <tye@zurich.ibm.com>
2019-11-26 10:14:38 +0100

    complete pseudo_label

--- histocartography/image/VorHoVerNet/pseudo_label.py
+++ histocartography/image/VorHoVerNet/pseudo_label.py
@@ -1,4 +1,4 @@
-from utils import booleanize_point_labels, get_point_from_instance
+from utils import get_point_from_instance, draw_boundaries
 from color_label import get_cluster_label
 from Voronoi_label import get_voronoi_edges
 
@@ -16,8 +16,45 @@ def gen_pseudo_label(image, point_mask):
     out_dict = {}
 
     distance_based_label = get_voronoi_edges(point_mask, extra_out=out_dict)
-    color_based_label = get_cluster_label(image, out_dict["dist_map"], point_mask, out_dict["Voronoi_cell"])
+    color_based_label = get_cluster_label(image, out_dict["dist_map"], point_mask, out_dict["Voronoi_cell"], distance_based_label)
 
-    # TODO: combine the labels
+    nuclei = (color_based_label == [0, 255, 0]).all(axis=2)
+
+    draw_boundaries(image, nuclei, color=[0, 255, 255])
+
+    color_based_label[nuclei] = [0, 0, 0]
+    from skimage.morphology import watershed, label
+    import numpy as np
+    from utils import get_gradient
+    labeled_nuclei = label(nuclei)
+    mask = np.zeros_like(nuclei)
+    for point in np.argwhere(point_mask):
+        point = tuple(point)
+        if labeled_nuclei[point] == 0: continue
+        mask = mask | (labeled_nuclei == labeled_nuclei[point])
+
+    draw_boundaries(image, mask)
+    
+    from skimage.io import imsave
+
+    imsave("img.png", image)
 
     return None
+
+if __name__ == "__main__":
+    from dataset_reader import CoNSeP
+    IDX = 6
+    SPLIT = 'test'
+    dataset = CoNSeP(download=False)
+    ori = dataset.read_image(IDX, SPLIT)
+    point_mask = dataset.read_points(IDX, SPLIT)
+    # point_mask = booleanize_point_labels(get_point_from_instance(dataset.read_labels(IDX, SPLIT)[0]))
+    from skimage.morphology import binary_dilation, disk
+    from skimage.io import imsave
+    import numpy as np
+
+    gen_pseudo_label(ori, point_mask)
+    point_mask = binary_dilation(point_mask, disk(3))
+    point_mask = np.where(point_mask, 255, 0)
+
+    imsave("points.png", point_mask)
