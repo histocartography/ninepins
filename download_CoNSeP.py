82d87a2ad25fd2be4c5663db18b9896a151590a8
Ting An Yen <tye@zurich.ibm.com>
2019-12-10 10:33:20 +0100

    move download script for importing module

--- download_CoNSeP.py
+++ download_CoNSeP.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-import ninepins.histocartography.io.utils as utils
+import histocartography.io.utils as utils
 
 BUCKET = "curated-datasets"
 DATASET = "colorectal/PATCH/CoNSeP"
-DATA_PATH = "../../../../data/"
+DATA_PATH = "../data/"
 
 utils.download_s3_dataset(
 	utils.get_s3(), BUCKET, DATASET, DATA_PATH
