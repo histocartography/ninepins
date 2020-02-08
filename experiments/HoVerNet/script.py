ec0fa8cdfbb2ffc9c0636e1aa3cc8958438b4073
Ting An Yen <fad11204@yahoo.com.tw>
2020-02-08 21:55:34 +0800

    add an experiment to eval hovernet performance

---
+++ experiments/HoVerNet/script.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+"""
+Script for evaluating HoverNet performance
+"""
+import logging
+import argparse
+import numpy as np
+import sys
+import os
+import mlflow
+from skimage.io import imsave
+from histocartography.image.VorHoVerNet.metrics import score, VALID_METRICS
+import histocartography.image.VorHoVerNet.dataset_reader as dataset_reader
+
+# setup logging
+# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+log = logging.getLogger('Histocartography::HoverNet')
+h1 = logging.StreamHandler(sys.stdout)
+log.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+h1.setFormatter(formatter)
+log.addHandler(h1)
+
+# configure argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-r',
+    '--dataset-root',
+    type=str,
+    help='root directory containing datasets',
+    default='../../histocartography/image/VorHoVerNet/',
+    required=False
+)
+parser.add_argument(
+    '-d',
+    '--dataset',
+    type=str,
+    help='dataset ([CoNSeP, MoNuSeg])',
+    default='CoNSeP',
+    choices=['CoNSeP', 'MoNuSeg'],
+    required=False
+)
+parser.add_argument(
+    '-p',
+    '--prediction-dir',
+    type=str,
+    help='root directory containing prediction npy files',
+    default='../../histocartography/image/VorHoVerNet/hover_net/',
+    required=False
+)
+def main(arguments):
+    """
+    Run HoverNet evaluation on the split of dataset
+    Args:
+        arguments (Namespace): parsed arguments.
+    """
+    # create aliases
+    DATASET_ROOT = arguments.dataset_root
+    DATASET = arguments.dataset
+    PREDICTION_DIR = arguments.prediction_dir
+
+    dataset = getattr(dataset_reader, DATASET)(download=False, root=DATASET_ROOT+DATASET+"/")
+
+    metrics = VALID_METRICS.keys()
+
+    aggregated_metrics = {}
+
+    for IDX in range(1, dataset.IDX_LIMITS['test'] + 1):
+        output_map = np.load(PREDICTION_DIR + f'test_{IDX}.npy')
+        label, _ = dataset.read_labels(IDX, 'test')
+        s = score(output_map, label, *metrics)
+
+        for metric in metrics:
+            value = s[metric]
+            if isinstance(value, dict):
+                for key, val in value.items():
+                    if not isinstance(val, list):
+                        metric_name = metric + '_' + key
+                        mlflow.log_metric(metric_name, val, step=(IDX-1))
+                        if metric_name not in aggregated_metrics:
+                            aggregated_metrics[metric_name] = []
+                        aggregated_metrics[metric_name].append(val) 
+            else:
+                mlflow.log_metric(metric, value, step=(IDX-1))
+                if metric not in aggregated_metrics:
+                    aggregated_metrics[metric] = []
+                aggregated_metrics[metric].append(value)
+
+    for metric, score_list in aggregated_metrics.items():
+        mlflow.log_metric("average_" + metric, sum(score_list) / len(score_list))
+
+if __name__ == "__main__":
+    main(arguments=parser.parse_args())
